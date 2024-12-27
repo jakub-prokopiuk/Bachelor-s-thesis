605496bb4c916ccbf7a7d5e44947bee691f27121
Jakub Prokopiuk <kubapro84@wp.pl>
2024-12-27 01:33:05 +0100

    added checking if accessToken exists in sharedPreferences so user doesnt have to log in every time

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'welcome_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'map_page.dart';
 
 void main() async {
   await dotenv.load();
-  runApp(const WattWay());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final accessToken = prefs.getString('access_token');
+
+  runApp(WattWay(accessToken: accessToken));
 }
 
 class WattWay extends StatelessWidget {
-  const WattWay({super.key});
+  final String? accessToken;
+
+  const WattWay({super.key, this.accessToken});
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +28,7 @@ class WattWay extends StatelessWidget {
         useMaterial3: true,
       ),
       themeMode: ThemeMode.system,
-      home: const WelcomePage(),
+      home: accessToken == null ? const WelcomePage() : const MapPage(),
     );
   }
 }
